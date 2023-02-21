7f61e77ca40e58da9db305c6d120975b3e473436
Roy Miara <miararoy@gmail.com>
2023-02-21 16:15:13 +0200

    values==>vector

--- pinecone_datasets/cfg.py
+++ pinecone_datasets/cfg.py
@@ -15,10 +15,10 @@ class Schema:
     }
     documents_select_columns = ["id", "values", "sparse_values", "metadata"]
     queries = {
-        "values": List(Float32),
-        "sparse_values": Struct(
+        "vector": List(Float32),
+        "sparse_vector": Struct(
             [Field("indices", List(UInt32)), Field("values", List(Float32))]
         ),
         "top_k": UInt32,
     }
-    queries_select_columns = ["values", "sparse_values", "filter", "top_k"]
+    queries_select_columns = ["vector", "sparse_vector", "filter", "top_k"]
