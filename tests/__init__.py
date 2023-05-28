05efc9d79dbb3e157496d05b0fce75a665c59f19
Roy Miara <miararoy@gmail.com>
2023-05-28 21:00:56 +0300

    change env names

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 
-if "AWS_ACCESS_KEY_ID" not in os.environ or "AWS_SECRET_ACCESS_KEY" not in os.environ:
+if "S3_ACCESS_KEY" not in os.environ or "S3_SECRET" not in os.environ:
     pytest.fail(
-        "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set as environment variables"
+        "S3_ACCESS_KEY and S3_SECRET must be set as environment variables"
     )
