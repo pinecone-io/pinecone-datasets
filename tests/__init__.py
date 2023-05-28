8d4c0978e8fbb479e5d335c3d7e46e38ec2488a8
Roy Miara <miararoy@gmail.com>
2023-05-28 21:03:13 +0300

    black

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,4 @@ import os
 import pytest
 
 if "S3_ACCESS_KEY" not in os.environ or "S3_SECRET" not in os.environ:
-    pytest.fail(
-        "S3_ACCESS_KEY and S3_SECRET must be set as environment variables"
-    )
+    pytest.fail("S3_ACCESS_KEY and S3_SECRET must be set as environment variables")
