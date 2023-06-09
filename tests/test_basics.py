7fca1a00b796d244bd0216a986e0d3e198425e16
Roy Miara <miararoy@gmail.com>
2023-06-09 21:16:37 +0300

    fix toml read

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,11 +3,15 @@ from pinecone_datasets import __version__
 
 if sys.version_info > (3, 11):
     import tomllib as toml
+
+    with open("pyproject.toml", "rb") as f:
+        assert toml.load(f)["tool"]["poetry"]["version"] == __version__
 else:
     import toml
+
+    with open("pyproject.toml") as f:
+        assert toml.load(f)["tool"]["poetry"]["version"] == __version__
 
 
 def test_version():
     assert __version__ == "0.5.0-rc.1"
-    with open("pyproject.toml") as f:
-        assert toml.load(f)["tool"]["poetry"]["version"] == __version__
