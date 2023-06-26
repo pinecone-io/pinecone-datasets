c3eb16e8df39817956e5571a6dca34573a06a319
Roy Miara <miararoy@gmail.com>
2023-06-26 21:07:45 +0300

    code cleanup

--- pinecone_datasets/testing.py
+++
@@ -1,7 +0,0 @@
-from pandas.testing import assert_frame_equal
-
-
-def assert_datasets_equal(ds1, ds2):
-    assert_frame_equal(ds1.documents, ds2.documents)
-    assert_frame_equal(ds1.queries, ds2.queries)
-    assert ds1.metadata == ds2.metadata
