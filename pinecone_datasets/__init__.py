df2dabc955c70605445f663a5878e866e11ed283
Roy Miara <miararoy@gmail.com>
2023-06-26 21:10:32 +0300

    remove ds utils leftovers

--- pinecone_datasets/__init__.py
+++ pinecone_datasets/__init__.py
@@ -7,4 +7,3 @@ __version__ = "0.5.0-rc.9"
 from .dataset import Dataset, DatasetInitializationError
 from .public import list_datasets, load_dataset
 from .catalog import DatasetMetadata, DenseModelMetadata
-from .ds_utils import import_documents_keys_from_blob_to_metadata
