e22c6863812d1e165823cc03bd272e32bd7e2bc3
Roy Miara <miararoy@gmail.com>
2023-06-26 16:14:29 +0300

    use smaller dataset on pinecone in concurrency 1

--- tests/test_io_pinecone.py
+++ tests/test_io_pinecone.py
@@ -8,15 +8,17 @@ from tests.test_public_datasets import deep_list_cmp
 
 
 def test_large_dataset_upsert_to_pinecone():
-    tested_dataset = "quora_all-MiniLM-L6-bm25"
+    tested_dataset = "quora_all-MiniLM-L6-bm25-100K"
     index_name = f"quora-index-{os.environ['PY_VERSION'].replace('.', '-')}"
+    dataset_size = 100000
+    dataset_dim = 384
 
     print(f"Testing dataset {tested_dataset} with index {index_name}")
     ds = load_dataset(tested_dataset)
 
     client = Client()
 
-    assert ds.documents.shape[0] == 522931
+    assert ds.documents.shape[0] == dataset_size
 
     if index_name in client.list_indexes():
         print(f"Deleting index {index_name}")
@@ -26,16 +28,16 @@ def test_large_dataset_upsert_to_pinecone():
             print(f"Waiting for index {index_name} to be deleted")
             time.sleep(5)
     try:
-        ds.to_pinecone_index(index_name=index_name, batch_size=300, concurrency=5)
+        ds.to_pinecone_index(index_name=index_name, batch_size=300, concurrency=1)
         index = client.get_index(index_name)
 
         assert index_name in client.list_indexes()
         assert client.describe_index(index_name).name == index_name
-        assert client.describe_index(index_name).dimension == 384
+        assert client.describe_index(index_name).dimension == dataset_dim
 
         # Wait for index to be ready
         time.sleep(60)
-        assert index.describe_index_stats().total_vector_count == 522931
+        assert index.describe_index_stats().total_vector_count == dataset_size
 
         assert deep_list_cmp(
             index.fetch(ids=["1"])["1"].values, ds.documents.loc[0].values[1].tolist()
