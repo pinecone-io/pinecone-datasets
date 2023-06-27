3c8cd281eacb852c2691380f0570940f4ec47708
Roy Miara <miararoy@gmail.com>
2023-06-27 11:31:21 +0300

    [skip ci] bump version

--- pinecone_datasets/__init__.py
+++ pinecone_datasets/__init__.py
@@ -2,7 +2,7 @@
 .. include:: ../README.md
 """
 
-__version__ = "0.5.0-rc.10"
+__version__ = "0.5.0-rc.11"
 
 from .dataset import Dataset, DatasetInitializationError
 from .public import list_datasets, load_dataset
