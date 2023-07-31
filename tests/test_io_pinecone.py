f653fffaf2fb13b86c085cedf7768d86c67e5526
Roy Miara <miararoy@gmail.com>
2023-07-31 15:04:51 +0300

    move to teardown method

--- tests/test_io_pinecone.py
+++ tests/test_io_pinecone.py
@@ -70,58 +70,57 @@ class TestPinecone:
                 self.client.delete_index(self.index_name)
 
     def test_dataset_upsert_to_existing_index(self):
-        try:
-            # create an index
-            this_test_index = self.index_name + "-precreated"
-            self.client.create_index(name=this_test_index, dimension=self.dataset_dim)
-            # check that index exists
-            assert this_test_index in self.client.list_indexes()
+        # create an index
+        this_test_index = self.index_name + "-precreated"
+        self.client.create_index(name=this_test_index, dimension=self.dataset_dim)
+        # check that index exists
+        assert this_test_index in self.client.list_indexes()
 
-            # check that index is empty
-            assert (
-                self.client.Index(this_test_index)
-                .describe_index_stats()
-                .total_vector_count
-                == 0
-            )
-            # upsert dataset to index
-            self.ds.to_pinecone_index(
-                index_name=this_test_index,
-                batch_size=300,
-                concurrency=1,
-                should_create=False,
-            )
-            index = self.client.Index(this_test_index)
+        # check that index is empty
+        assert (
+            self.client.Index(this_test_index)
+            .describe_index_stats()
+            .total_vector_count
+            == 0
+        )
+        # upsert dataset to index
+        self.ds.to_pinecone_index(
+            index_name=this_test_index,
+            batch_size=300,
+            concurrency=1,
+            should_create=False,
+        )
+        index = self.client.Index(this_test_index)
 
-            # Wait for index to be ready
-            time.sleep(60)
-            assert index.describe_index_stats().total_vector_count == self.dataset_size
-            assert (
-                index.describe_index_stats().namespaces[""].vector_count
-                == self.dataset_size
-            )
+        # Wait for index to be ready
+        time.sleep(60)
+        assert index.describe_index_stats().total_vector_count == self.dataset_size
+        assert (
+            index.describe_index_stats().namespaces[""].vector_count
+            == self.dataset_size
+        )
 
-            # upsert dataset to index at a specific namespace
-            namespace = "test"
-            self.ds.to_pinecone_index(
-                index_name=this_test_index,
-                batch_size=300,
-                concurrency=1,
-                should_create=False,
-                namespace=namespace,
-            )
+        # upsert dataset to index at a specific namespace
+        namespace = "test"
+        self.ds.to_pinecone_index(
+            index_name=this_test_index,
+            batch_size=300,
+            concurrency=1,
+            should_create=False,
+            namespace=namespace,
+        )
 
-            # Wait for index to be ready
-            time.sleep(60)
-            assert (
-                index.describe_index_stats().total_vector_count == self.dataset_size * 2
-            )
-            assert (
-                index.describe_index_stats().namespaces[namespace].vector_count
-                == self.dataset_size
-            )
+        # Wait for index to be ready
+        time.sleep(60)
+        assert (
+            index.describe_index_stats().total_vector_count == self.dataset_size * 2
+        )
+        assert (
+            index.describe_index_stats().namespaces[namespace].vector_count
+            == self.dataset_size
+        )
 
-        finally:
-            if this_test_index in self.client.list_indexes():
-                print(f"Deleting index {this_test_index}")
-                self.client.delete_index(this_test_index)
+    def teardown_method(self):
+        if self.index_name in self.client.list_indexes():
+            print(f"Deleting index {self.index_name}")
+            self.client.delete_index(self.index_name)
